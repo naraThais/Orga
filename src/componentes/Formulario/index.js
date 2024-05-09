c4612a0a3e941e2782f02aff2802349d6be178d8
Thais Nara <nara.silva@grendene.com>
2024-05-09 16:57:16 -0300

    Function Salvar

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -16,15 +16,20 @@ const Formulario = () => {
     ]
 
 
+const aoSalvar = (evento) => {
+    evento.preventDefault()
+    console.log('Form foi submetido')
+}
+
 
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto  label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto  obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-                <ListaSuspensa label="Time" itens={times}/>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
                 <Botao>Criar Card</Botao>
             </form>
         </section>
